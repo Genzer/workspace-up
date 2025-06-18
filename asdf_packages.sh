50c84452b38a6c05faece7f3d2ebc11fa6c764c4
Genzer <732937+Genzer@users.noreply.github.com>
2025-06-18 15:14:05 +0700

    Add asdf dump

---
+++ asdf_packages.sh
@@ -0,0 +1,70 @@
+# OS:
+# Darwin MAC-C02GJ2YEMD6R 24.5.0 Darwin Kernel Version 24.5.0: Tue Apr 22 19:53:26 PDT 2025; root:xnu-11417.121.6~2/RELEASE_X86_64 x86_64 i386 Darwin
+# 
+# SHELL:
+# zsh 5.9 (x86_64-apple-darwin24.0)
+# 
+# BASH VERSION:
+# 5.2.37(1)-release
+# 
+# ASDF VERSION:
+# v0.14.1
+# 
+# ASDF INTERNAL VARIABLES:
+# ASDF_DEFAULT_TOOL_VERSIONS_FILENAME=.tool-versions
+# ASDF_DATA_DIR=/Users/hautran/.asdf
+# ASDF_DIR=/usr/local/opt/asdf/libexec
+# ASDF_CONFIG_FILE=/Users/hautran/.asdfrc
+# 
+# ASDF INSTALLED PLUGINS:
+# aws-sam-cli                  git@github.com:amrox/asdf-pyapp.git master 41f5275
+# golang                       git@github.com:asdf-community/asdf-golang.git master 0e86b1d
+# graalvm                      git@github.com:asdf-community/asdf-graalvm.git master cf137f6
+# hugo                         git@github.com:NeoHsu/asdf-hugo.git master 125ce37
+# java                         git@github.com:halcyon/asdf-java.git master bba0977
+# kotlin                       git@github.com:asdf-community/asdf-kotlin.git master 94610b0
+# nodejs                       git@github.com:asdf-vm/asdf-nodejs.git master c5b7c40
+# pnpm                         git@github.com:jonathanmorley/asdf-pnpm.git master 184f8cb
+# python                       git@github.com:danhper/asdf-python.git master 5e277e2
+# terraform                    git@github.com:asdf-community/asdf-hashicorp.git master f1602b6
+# 
+asdf plugin add aws-sam-cli
+asdf plugin add golang
+asdf plugin add graalvm
+asdf plugin add hugo
+asdf plugin add java
+asdf plugin add kotlin
+asdf plugin add nodejs
+asdf plugin add pnpm
+asdf plugin add python
+asdf plugin add terraform
+asdf install golang 1.20.2
+asdf install golang 1.23.4
+asdf install graalvm 22.3.3-java11
+asdf install hugo 0.100.0
+asdf install hugo 0.114.0
+asdf install hugo 0.120.4
+asdf install hugo 0.121.0
+asdf install hugo 0.129.0
+asdf install hugo 0.130.0
+asdf install hugo extended_0.121.0
+asdf install hugo extended_0.130.0
+asdf install java adoptopenjdk-8.0.252+9.1
+asdf install java adoptopenjdk-8.0.362+9
+asdf install java temurin-11.0.16+8
+asdf install java temurin-17.0.6+10
+asdf install java temurin-22.0.0+36
+asdf install java zulu-jre-javafx-8.64.0.19
+asdf install kotlin 2.0.20
+asdf install nodejs 16.18.1
+asdf install nodejs 16.9.1
+asdf install nodejs 18.19.0
+asdf install nodejs 22.1.0
+asdf install nodejs 22.4.1
+asdf install pnpm 9.11.0
+asdf install python 3.12.5
+asdf install python 3.13.1
+asdf install python 3.6.15
+asdf install terraform 1.11.3
+asdf install terraform 1.8.3
+asdf install terraform 1.8.5
